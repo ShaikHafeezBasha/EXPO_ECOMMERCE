cae5019d8b92cddeed8b599c77de2dafa19ed1d1
shaikhafeezbasha <shaikhafeez500@gmail.com>
2026-02-04 22:24:07 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,24 @@
 import express from "express";
-import {ENV} from "./config/ENV.js";
+import { ENV } from "./config/ENV.js";
 import { connectDB } from "./config/db.js";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { functions, inngest } from "./config/inngest.js";
 
 const app = express();
 
-app.use(clerkMiddleware())
+app.use(express.json());
+app.use(clerkMiddleware());
 
-app.get("/api/health",(req,res)=>{
-    res.status(200).json({message:"Success"});
-})
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.listen(ENV.PORT,()=>{
-    connectDB();
-    console.log("Server is up and running");
-})
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "Success" });
+});
 
+app.listen(ENV.PORT, () => {
+  connectDB();
+  console.log("Server is up and running");
+});
 
 // https://expo-ecommerce-a559.onrender.com/
