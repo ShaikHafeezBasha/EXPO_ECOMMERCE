c90a931300009f8022fc2f7e7057cfb0f6f4dc85
shaikhafeezbasha <shaikhafeez500@gmail.com>
2026-02-04 20:57:30 +0530

    db config & tools setup

--- backend/src/config/ENV.js
+++ backend/src/config/ENV.js
@@ -5,5 +5,11 @@ dotenv.config();
 
 export const ENV = {
     PORT : process.env.PORT,
-    DB_URI : process.env.DB_URI
+    DB_URL : process.env.DB_URL,
+    CLERK_PUBLISHABLE_KEY : process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY : process.env.CLERK_SECRET_KEY,
+    INNGEST_SIGNING_KEY : process.env.INNGEST_SIGNING_KEY,
+    CLOUDINARY_API_KEY : process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET : process.env.CLOUDINARY_API_SECRET,
+    CLOUDINARY_CLOUD_NAME : process.env.CLOUDINARY_CLOUD_NAME
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,20 @@
 import express from "express";
-import {ENV} from "./config/ENV.js";
+import {ENV} from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
+
+app.use(clerkMiddleware())
 
 app.get("/api/health",(req,res)=>{
     res.status(200).json({message:"Success"});
 })
 
 app.listen(ENV.PORT,()=>{
+    connectDB();
     console.log("Server is up and running");
 })
+
+
+// https://expo-ecommerce-a559.onrender.com/
