69f17f41dcc6af6688c3834c56a719be6a01810e
shaikhafeezbasha <shaikhafeez500@gmail.com>
2026-02-07 23:41:19 +0530

    added models

---
+++ backend/src/models/cart.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const cartItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    ref: "Product",
+    min: 1,
+    default: 1,
+  },
+});
+
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    clerkId: {
+      type: string,
+      required: true,
+      unique: true,
+    },
+    items: [cartItemSchema],
+  },
+  { timestamps: true },
+);
+
+export const Cart = mongoose.model("Cart", cartSchema);
